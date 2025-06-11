7f4736ee8c9d8ac28492faaeba81d8ee0e473156
Alejandro Lopez <lopalejandro@gmail.com>
2025-06-11 10:44:45 -0300

    Primera subida

---
+++ genera.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Generando de 1 a 10000"
+php migra.php ./base/10000.txt 10000.xml >./logs/10000.log
+echo "Generando de 10001 a 20000"
+php migra.php ./base/20000.txt 20000.xml >./logs/20000.log
+echo "Generando de 20001 a 30000"
+php migra.php ./base/30000.txt 30000.xml >./logs/30000.log
+echo "Generando de 30001 a 40000"
+php migra.php ./base/40000.txt 40000.xml >./logs/40000.log
+echo "Generando de 40001 a 50000"
+php migra.php ./base/50000.txt 50000.xml >./logs/50000.log
+echo "Generando de 50001 a 60000"
+php migra.php ./base/60000.txt 60000.xml >./logs/60000.log
+echo "Generando de 60001 a 70000"
+php migra.php ./base/70000.txt 70000.xml >./logs/70000.log
+echo "Generando de 70001 a 80000"
+php migra.php ./base/80000.txt 80000.xml >./logs/80000.log
+echo "Generando de 80001 a 90000"
+php migra.php ./base/90000.txt 90000.xml >./logs/90000.log
+echo "Generando de 90001 a 100000"
+php migra.php ./base/100000.txt 100000.xml >./logs/100000.log
+echo "Generando de 100001 a 110000"
+php migra.php ./base/110000.txt 110000.xml >./logs/110000.log
+echo "Generando de 110001 a 120000"
+php migra.php ./base/120000.txt 120000.xml >./logs/120000.log
+echo "Generando de 120001 a 130000"
+php migra.php ./base/130000.txt 130000.xml >./logs/130000.log
+echo "Generando de 130001 a 140000"
+php migra.php ./base/140000.txt 140000.xml >./logs/140000.log
+
